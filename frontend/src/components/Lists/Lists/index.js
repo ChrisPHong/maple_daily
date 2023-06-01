2a42045f4005f60d9d8ada8c6482a07dbc413fef
chris <Chrispyohong@gmail.com>
2023-06-01 01:15:20 -0700

    added CSS to the navbar

--- frontend/src/components/Lists/Lists/index.js
+++ frontend/src/components/Lists/Lists/index.js
@@ -3,38 +3,32 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { getUserLists } from "../../../store/list";
 import { getBosses } from "../../../store/boss";
-import OneList from '../OneList'
-import './Lists.css'
-
+import OneList from "../OneList";
+import "./Lists.css";
 
 const Lists = () => {
   const userId = useSelector((state) => state?.session?.user?.id);
-  const lists = useSelector((state) => Object.values(state?.listReducer?.lists));
+  const lists = useSelector((state) =>
+    Object.values(state?.listReducer?.lists)
+  );
   const dispatch = useDispatch();
   const history = useHistory();
 
   useEffect(() => {
-    if(userId){
-      dispatch(getUserLists({userId}));
-
+    if (userId) {
+      dispatch(getUserLists({ userId }));
     }
-
   }, [dispatch, userId]);
 
   return (
     <div className="All-lists-Container">
-      <button onClick={()=>{history.push('/createlist')}}>Create a list</button>
-    {
-      lists.map(list =>{
-      return (
-        <div key={list.id}>
-
-        <OneList props={list}/>
-
-        </div>
-      )
-     })
-    }
+      {lists.map((list) => {
+        return (
+          <div key={list.id}>
+            <OneList props={list} />
+          </div>
+        );
+      })}
     </div>
   );
 };
