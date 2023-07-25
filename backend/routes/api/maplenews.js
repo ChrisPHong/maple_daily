6175a0975b6258c796687fef8f3c82e40a2d39eb
chris <Chrispyohong@gmail.com>
2023-07-25 11:44:19 -0700

    changed the input

--- backend/routes/api/maplenews.js
+++ backend/routes/api/maplenews.js
@@ -17,7 +17,9 @@ const validateSignup = [];
 router.get(
   "/",
   asyncHandler(async (req, res) => {
-    const browser = await puppeteer.launch(`headless: "new"`);
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
     await page.goto("https://maplestory.nexon.net/news");
 
