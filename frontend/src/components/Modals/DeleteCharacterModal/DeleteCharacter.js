8e316488af2b8f93e323e9baad5fd29161bc2f68
chris <Chrispyohong@gmail.com>
2023-10-18 13:36:31 -0700

    added another feature

--- frontend/src/components/Modals/DeleteCharacterModal/DeleteCharacter.js
+++ frontend/src/components/Modals/DeleteCharacterModal/DeleteCharacter.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { deletingDashboardList } from "../../../store/list";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import './deleteCharacter.css'
 
 const DeleteCharacter = ({
   id,
@@ -23,16 +24,39 @@ const DeleteCharacter = ({
     await setShowModal(false);
     await history.push("/");
   };
+
   useEffect(() => {
     setChosenId(chosenId);
   }, [chosenId]);
+
   return (
-    <div className="flex flex-col">
-      <button className="cancel-drag-btn "
+    <div className="flex flex-col justify-center items-center">
+      <button className="cancel-drag-btn"
         onClick={async (e) => {
           e.preventDefault()
           await setShowModal(false)
         }}>X</button>
+      {chosen.length > 0 ?
+        <div className={chosen.length > 0 ? "flex flex-col justify-center items-center bg-white p-4 rounded-md mb-3" : ''} >
+          <div className="font-bold">
+            You've selected <span className="font-bold underline">
+              {`${chosen[0]?.character} `}
+            </span>
+            to be deleted
+          </div>
+          <div
+            key={chosen[0]?.character}
+            className={`flex flex-row bg-blue-300 justify-between bg-blue-600
+            pl-5 items-center border-black border w-72 rounded-sm mt-2`}
+          >
+            <span className="drag-list-name">{chosen[0]?.character}</span>
+            <div
+              style={{ backgroundImage: `url(${chosen[0]?.apiContent})` }}
+              className="characterImage-drag"
+            ></div>
+          </div>
+        </div>
+        : ''}
       <div className="flex flex-col justify-center bg-white p-4 rounded-md">
         <div className="max-h-96 overflow-y-scroll border-black border-2 rounded-lg">
           {lists?.map((list, idx) => {
