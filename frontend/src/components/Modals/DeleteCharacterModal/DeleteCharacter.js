c21f399b52881734fa006b14a4b0e8d41f7118ab
chris <Chrispyohong@gmail.com>
2023-10-18 11:56:23 -0700

    added the close modal button for the delete modal

--- frontend/src/components/Modals/DeleteCharacterModal/DeleteCharacter.js
+++ frontend/src/components/Modals/DeleteCharacterModal/DeleteCharacter.js
@@ -27,38 +27,44 @@ const DeleteCharacter = ({
     setChosenId(chosenId);
   }, [chosenId]);
   return (
-    <div className="flex flex-col justify-center bg-white p-4 rounded-md">
-      <div className="max-h-96 overflow-y-scroll border-black border-2 rounded-lg">
-        {lists?.map((list, idx) => {
-          return (
-            <div
-              key={list.character}
-              className={`flex flex-row bg-blue-300 justify-between ${
-                chosenId === list.id ? "bg-blue-600" : "bg-blue-300"
-              } pl-5 items-center border-black border hover:cursor-pointer hover:bg-blue-600 w-72`}
-              onClick={(e) => {
-                e.preventDefault();
-                setChosen([list]);
-                setChosenId(list.id);
-              }}
-            >
-              <span className="drag-list-name">{list.character}</span>
-              <div
-                style={{ backgroundImage: `url(${list.apiContent})` }}
-                className="characterImage-drag"
-              ></div>
-            </div>
-          );
-        })}
-      </div>
-      <button
-        className="p-3 rounded-full border-2 border-black mt-2 text-white font-bold delete-bg"
+    <div className="flex flex-col">
+      <button className="cancel-drag-btn "
         onClick={async (e) => {
-          await onSubmit(e);
-        }}
-      >
-        Delete X
-      </button>
+          e.preventDefault()
+          await setShowModal(false)
+        }}>X</button>
+      <div className="flex flex-col justify-center bg-white p-4 rounded-md">
+        <div className="max-h-96 overflow-y-scroll border-black border-2 rounded-lg">
+          {lists?.map((list, idx) => {
+            return (
+              <div
+                key={list.character}
+                className={`flex flex-row bg-blue-300 justify-between ${chosenId === list.id ? "bg-blue-600" : "bg-blue-300"
+                  } pl-5 items-center border-black border hover:cursor-pointer hover:bg-blue-600 w-72`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setChosen([list]);
+                  setChosenId(list.id);
+                }}
+              >
+                <span className="drag-list-name">{list.character}</span>
+                <div
+                  style={{ backgroundImage: `url(${list.apiContent})` }}
+                  className="characterImage-drag"
+                ></div>
+              </div>
+            );
+          })}
+        </div>
+        <button
+          className="p-3 rounded-full border-2 border-black mt-2 text-white font-bold delete-bg"
+          onClick={async (e) => {
+            await onSubmit(e);
+          }}
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
